58c16b0fd0b328230144e0cb27135c99242abacb
Patrick Holton <patrickholton@Patricks-MBP.lan>
2024-03-15 18:12:12 -0400

    home page

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="container flex items-center text-2xl font-bold text-primary">
+      Video Drive
+    </div>
+  );
+};
+
+export default Page;
