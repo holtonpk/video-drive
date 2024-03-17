a5e2ff5db4128340af4292aa2543e172bdfe813d
Patrick Holton <patrickholton@Patricks-MBP.lan>
2024-03-17 14:26:13 -0400

    snap

--- src/app/video-sheet/page.tsx
+++ src/app/video-sheet/page.tsx
@@ -10,7 +10,14 @@ import {columns} from "./components/columns";
 import {DataTable} from "./components/data-table";
 import {Video} from "./data/schema";
 import {db} from "@/config/firebase";
-import {doc, getDocs, collection, query, orderBy} from "firebase/firestore";
+import {
+  doc,
+  getDocs,
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
 
 import {buttonVariants} from "@/components/ui/button";
 import Link from "next/link";
@@ -19,26 +26,45 @@ import {use, useEffect} from "react";
 
 const url = process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000";
 
-async function getVideos() {
-  const videos = await fetch(`${url}/api/videos`, {
-    cache: "no-store",
-  });
+// async function getVideos() {
+//   const videos = await fetch(`${url}/api/videos`, {
+//     cache: "no-store",
+//   });
 
-  const videoData = await videos.json();
+//   const videoData = await videos.json();
 
-  return videoData as Video[];
-}
+//   return videoData as Video[];
+// }
 
 export default function VideoSheetPage() {
   const [videos, setVideos] = React.useState<Video[]>([]);
 
   useEffect(() => {
-    getVideos().then((data) => {
-      setVideos(data);
-    });
-  }, []);
+    const docsQuery = query(
+      collection(db, "videos"),
+      orderBy("dueDate", "desc")
+    );
 
-  console.log("videos", videos);
+    const unsubscribe = onSnapshot(docsQuery, (querySnapshot) => {
+      const videos: Video[] = [];
+      querySnapshot.forEach((doc) => {
+        videos.push(doc.data() as Video);
+      });
+
+      // Custom sort function to order by status
+      videos.sort((a, b) => {
+        const statusOrder = ["needs revision", "in progress", "todo", "done"];
+        const indexA = statusOrder.indexOf(a.status.toLowerCase());
+        const indexB = statusOrder.indexOf(b.status.toLowerCase());
+        return indexA - indexB;
+      });
+
+      setVideos(videos);
+    });
+
+    // Cleanup the subscription on unmount
+    return () => unsubscribe();
+  }, []);
 
   return (
     <>
@@ -58,7 +84,7 @@ export default function VideoSheetPage() {
             New Video
           </Link>
         </div>
-        {videos ? (
+        {videos && videos.length > 0 ? (
           <div className="p-4 border rounded-md">
             <DataTable data={videos} columns={columns} />
           </div>
