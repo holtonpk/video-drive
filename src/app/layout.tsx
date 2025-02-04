c36097d8405cabde3fb5058b68e45d4253f9a342
Patrick Holton <patrickholton@Patricks-MBP.lan>
2025-02-03 18:24:28 -0600

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 // import "../../global-style.css";
 import {SpeedInsights} from "@vercel/speed-insights/next";
+import {Analytics} from "@vercel/analytics/react";
 export const metadata = {
   title: "Ripple Media",
   description: "Experts in short form",
@@ -9,6 +10,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <SpeedInsights />
+      <Analytics />
       <body>{children}</body>
     </html>
   );
