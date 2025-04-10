5de197d5d201d52c57170a2dcce99c0bd4b8b8f2
Patrick Holton <patrickholton@MacBookPro.lan>
2025-04-09 20:04:19 -0500

    treecard

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,19 @@ const nextConfig = {
 
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+    domains: [
+      "p19-sign.tiktokcdn-us.com",
+      "p19-pu-sign-useast8.tiktokcdn-us.com",
+      "p16-pu-sign-useast8.tiktokcdn-us.com",
+    ],
+  },
 };
 
 export default nextConfig;
