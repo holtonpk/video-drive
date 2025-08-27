8f15e8b67518cf24737a769dda7925537b4c274e
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2025-08-27 10:24:17 -0600

    week 13

--- src/app/(tool)/reports/blueCollarKeys/report-page.tsx
+++ src/app/(tool)/reports/blueCollarKeys/report-page.tsx
@@ -699,7 +699,7 @@ Content is regularly going viral. We’re seeing strong consistency in reach and
       data: instagramData,
       icon: InstagramLogo,
       name: "Instagram",
-      followers: 54,
+      followers: 61,
       posts: instagramData.length,
       totalViews:
         Number(
