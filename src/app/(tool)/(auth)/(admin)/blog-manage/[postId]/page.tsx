315550f3b481348508af6e497bff3df614569692
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2025-09-11 11:46:59 -0600

    page

--- src/app/(tool)/(auth)/(admin)/blog-manage/[postId]/page.tsx
+++ src/app/(tool)/(auth)/(admin)/blog-manage/[postId]/page.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import EditorLayout from "./editor-layout";
 import "./blog-style.css";
+
 interface EditorPageProps {
   params: {postId: string};
 }
 
-const EditorPage = ({params}: EditorPageProps) => {
+export default function EditorPage({params}: EditorPageProps) {
   return <EditorLayout postId={params.postId} />;
-};
-
-export default EditorPage;
+}
