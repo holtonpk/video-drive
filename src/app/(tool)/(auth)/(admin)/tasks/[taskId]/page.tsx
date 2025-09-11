fae4154e6f681337fa6e5e28e5e105db2cbc17df
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2025-09-11 12:15:00 -0600

    a

--- src/app/(tool)/(auth)/(admin)/tasks/[taskId]/page.tsx
+++ src/app/(tool)/(auth)/(admin)/tasks/[taskId]/page.tsx
@@ -1,23 +1,8 @@
 "use client";
-import {constructMetadata} from "@/lib/utils";
+
 import {Metadata} from "next";
 import Task from "./task";
 import {useRouter} from "next/router";
-export const generateMetadata = ({
-  params,
-}: {
-  params: {taskId: string};
-}): Metadata => {
-  return {
-    title: `Task - ${params.taskId}`,
-    description: "Agency Video Sheet",
-    icons: {
-      icon: "image/favicon.ico",
-      shortcut: "image/favicon-16x16.png",
-      apple: "image/apple-touch-icon.png",
-    },
-  };
-};
 
 const Page = () => {
   const router = useRouter();
