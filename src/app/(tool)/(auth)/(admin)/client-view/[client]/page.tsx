d164596ca27321ecdb1b33e98ce9eec1afbb0b47
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2025-09-13 21:46:21 -0600

    shit

--- src/app/(tool)/(auth)/(admin)/client-view/[client]/page.tsx
+++ src/app/(tool)/(auth)/(admin)/client-view/[client]/page.tsx
@@ -5,24 +5,24 @@ import ClientPage from "./client-page";
 import {clients} from "@/config/data";
 import {useRouter} from "next/router";
 
-export const generateMetadata = ({
-  params,
-}: {
-  params: {client: string};
-}): Metadata => {
-  const {client} = params;
-  const clientInfo = clients.find((c: any) => c.value === client);
+// export const generateMetadata = ({
+//   params,
+// }: {
+//   params: {client: string};
+// }): Metadata => {
+//   const {client} = params;
+//   const clientInfo = clients.find((c: any) => c.value === client);
 
-  return {
-    title: `${clientInfo?.label} | Video Planning`,
-    description: "Agency Video Sheet",
-    icons: {
-      icon: `${process.env.NEXT_PUBLIC_SITE_URL}/image/favicon.ico`,
-      shortcut: `${process.env.NEXT_PUBLIC_SITE_URL}/image/favicon.ico`,
-      apple: `${process.env.NEXT_PUBLIC_SITE_URL}/image/apple-touch-icon.png`,
-    },
-  };
-};
+//   return {
+//     title: `${clientInfo?.label} | Video Planning`,
+//     description: "Agency Video Sheet",
+//     icons: {
+//       icon: `${process.env.NEXT_PUBLIC_SITE_URL}/image/favicon.ico`,
+//       shortcut: `${process.env.NEXT_PUBLIC_SITE_URL}/image/favicon.ico`,
+//       apple: `${process.env.NEXT_PUBLIC_SITE_URL}/image/apple-touch-icon.png`,
+//     },
+//   };
+// };
 
 const Page = () => {
   const router = useRouter();
