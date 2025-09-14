3eb9e2a17e459eae254863fdd0950d9f4421c5a1
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2025-09-13 22:34:36 -0600

    paf

--- src/app/(tool)/(auth)/(admin)/client-view/[client]/client-page.tsx
+++ src/app/(tool)/(auth)/(admin)/client-view/[client]/client-page.tsx
@@ -25,7 +25,9 @@ const ClientPage = ({client}: {client: string}) => {
 
   return (
     <>
-      {clientInfo ? (
+      this is client page
+      {clientInfo?.label}
+      {/* {clientInfo ? (
         <div className={displayedVideo ? `grid md:grid-cols-[60%_1fr]` : ""}>
           <ScrollArea className=" flex flex-col w-full gap-4  relative h-[calc(100vh-64px)]  overflow-scroll pt-20 ">
             <div className="max-w-full w-full absolute top-0 h-16  z-20 flex justify-between container">
@@ -48,8 +50,6 @@ const ClientPage = ({client}: {client: string}) => {
                   </span>
                 )}
               </div>
-              {/* <VectorizeData clientInfo={clientInfo} />
-              <ExportData clientInfo={clientInfo} /> */}
               <div className="flex w-fit bg-card p-1 rounded-md border h-fit ml-auto mr-4">
                 <button
                   onClick={() => setView("week")}
@@ -126,7 +126,7 @@ const ClientPage = ({client}: {client: string}) => {
         </div>
       ) : (
         <>Not found</>
-      )}
+      )} */}
     </>
   );
 };
