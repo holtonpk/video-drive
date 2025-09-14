5f24ae7623964d4905fce78abf737c50f1466403
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2025-09-13 22:23:02 -0600

    paf

--- src/app/(tool)/(auth)/(admin)/client-view/[client]/client-page.tsx
+++ src/app/(tool)/(auth)/(admin)/client-view/[client]/client-page.tsx
@@ -1,14 +1,6 @@
 "use client";
 import React from "react";
 import {clients, VideoData} from "@/config/data";
-import {CreateVideo} from "./components/create-videos/create-videos";
-import {WeeksDisplay} from "./components/weeks-display2";
-import {VideoDisplay} from "./components/video-display";
-import {ScrollArea} from "@/components/ui/scroll-area";
-import ExportData from "./components/export";
-import {FeedPreview} from "./components/feed-preview";
-import {UnPostedDisplay} from "./components/unPosted-display";
-import {CalenderView} from "./components/calender-view";
 
 const ClientPage = ({client}: {client: string}) => {
   const clientInfo = clients.find((c: any) => c.value === client);
