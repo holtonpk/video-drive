6d366e3c02dd0f7595c8fd9ca7ff51570c1c5b70
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2025-09-19 13:28:14 -0600

    dashboard

--- src/app/(tool)/(auth)/(editors)/dashboard/page.tsx
+++ src/app/(tool)/(auth)/(editors)/dashboard/page.tsx
@@ -1,11 +1,12 @@
+"use client";
 import React from "react";
 import EditDashboard from "./editor-dashboard";
 import {constructMetadata} from "@/lib/utils";
 
-export const metadata = constructMetadata({
-  title: "Editor Dashboard",
-  description: "We specialize in organic marketing",
-});
+// export const metadata = constructMetadata({
+//   title: "Editor Dashboard",
+//   description: "We specialize in organic marketing",
+// });
 
 const Page = () => {
   return <EditDashboard />;
