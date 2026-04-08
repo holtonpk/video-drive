b7bf2b92628d9ebcbe65b06d13fa29975549dc80
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2026-04-08 12:04:57 +0800

    safari sucks

--- src/app/(marketing)/launch-library/(auth)/[slug]/scrollToTop.tsx
+++ src/app/(marketing)/launch-library/(auth)/[slug]/scrollToTop.tsx
@@ -3,7 +3,7 @@
 import {useEffect} from "react";
 import React from "react";
 
-export default function scrollToTop() {
+export default function ScrollToTop() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
